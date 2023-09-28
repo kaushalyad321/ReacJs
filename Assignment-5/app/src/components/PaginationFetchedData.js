067053678a54c05b891bb031f0a51e03689e5103
Kaushal Kumar Yadav <kaushal.yadav@successive.tech>
2023-09-28 12:37:24 +0530

    changed

--- Assignment-5/app/src/components/PaginationFetchedData.js
+++ Assignment-5/app/src/components/PaginationFetchedData.js
@@ -5,8 +5,7 @@ function PaginationFetchedData() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-   
-    fetch("https://official-joke-api.appspot.com/random_joke") 
+    fetch("https://swapi.dev/api/films")
       .then((response) => response.json())
       .then((data) => {
         setData(data);
@@ -16,25 +15,30 @@ function PaginationFetchedData() {
         console.error("Error fetching data:", error);
         setLoading(false);
       });
-  }, []); 
+  }, []);
 
+  // console.log(data["results"]);
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div>
+    <div style={{alignItems:'start'}}>
       <h1>Data fetched from API</h1>
-     {
-      Object.keys(data).map( (key)=> {
-              return <div key={key}>
-                 <span>{key}:</span>
-                 <span>{data[key]}</span>
-              </div>
-            })
-            }
-     
+      {Object.keys(data["results"]).map((key) => {
+        const objData = data["results"][key];
+        
+        const result = Object.keys(objData).map((key2) => {
+          return (
+            <div > 
+              <span style={{fontSize:'35px' ,color:'red',marginTop:'20px'}}>{key2}:</span><span style={{fontSize:'30px'}}>{objData[key2]}</span>
+            </div>
+          );
+        });
+        return <div  style={{backgroundColor:'grey',marginTop:'30px',textAlign:'start'}}>{result}</div>;
+        
+      })}
     </div>
   );
 }
