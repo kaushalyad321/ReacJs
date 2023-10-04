56b953465760cc32846217984ee5feeb985f38ce
Kaushal Kumar Yadav <kaushal.yadav@successive.tech>
2023-10-04 16:25:01 +0530

    removed unused style

--- Assignment-4/app/src/Components/DropDownMenu.js
+++ Assignment-4/app/src/Components/DropDownMenu.js
@@ -1,30 +1,30 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
 const DropDownMenu = () => {
-    const[option,setOption]=useState('') ;
-    let result='' ;
-    if(option.length>0){
-        result= <p>You have selected {option} color from the option</p>
-    }
- 
-    return(
+  const [option, setOption] = useState("");
+  let result = "";
+  if (option.length > 0) {
+    result = <p>You have selected {option} color from the option</p>;
+  }
+
+  return (
     <div>
       <form>
-        <select onClick={(e)=>{setOption(e.target.value)}}>
+        <select
+          onClick={(e) => {
+            setOption(e.target.value);
+          }}
+        >
           <option value="" selected disabled>
             Select a Color
           </option>
           <option value="blue">Blue</option>
           <option value="green">Green</option>
-          <option value="red" style={{}}>
-            Red
-          </option>
-          <option value="yellow" style={{ backgroundColor: "yellow" }}>Yellow</option>
-         </select>
+          <option value="red">Red</option>
+          <option value="yellow">Yellow</option>
+        </select>
       </form>
-      {
-        result
-      }
+      {result}
     </div>
   );
 };
